70ac1cf19a6d260b2d3e5787e2b3dbec94529a69
Michael Jerred <mjerred.work@gmail.com>
2020-12-04 16:04:45 +0000

    fix: correct the indicies

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ import h5py
 
 def load_cat_data():
     train_data = h5py.File('data/train_catvnoncat.h5', "r")
-    train_x = np.array(train_data["train_set_x"][:])
-    train_y = np.array(train_data["train_set_y"][:])
+    train_x = np.array(train_data["train_set_x"][:]) # shape: (209, 64, 64, 3)
+    train_y = np.array([train_data["train_set_y"][:]]).T # shape: (209, 1)
 
     test_data = h5py.File('data/test_catvnoncat.h5', "r")
-    test_x = np.array(test_data["test_set_x"][:])
-    test_y = np.array(test_data["test_set_y"][:])
+    test_x = np.array(test_data["test_set_x"][:]) # shape: (50, 64, 64, 3)
+    test_y = np.array([test_data["test_set_y"][:]]).T # shape: (50, 1)
 
     classes = np.array(test_data["list_classes"][:])
-
-    train_y = train_y.reshape((1, train_y.shape[0]))
-    test_y = test_y.reshape((1, test_y.shape[0]))
 
     return train_x, train_y, test_x, test_y, classes
 
@@ -29,7 +26,8 @@ def load_cat_data():
 # self.test_x = flat_test_x/flat_test_x.max()
 
 class BabyNet:
-    """Simple Neural Net; default at 2 layers with each hiddem layer at 8 nodes and the final layer at 1 node.
+    """
+        Simple Neural Net; default at 2 layers with each hiddem layer at 8 nodes and the final layer at 1 node.
         Using Relu activation at each layer except for the final layer, which is a sigmoid by default.
         Default cost function set to squared error cost.
     """
@@ -37,20 +35,30 @@ class BabyNet:
     def __init__(
         self,
         layer_dimensions = [12288,8,1],
-        activation = None,
-        activation_deriv = None,
+        activations = None,
+        activation_derivs = None,
         cost = None,
         cost_deriv = None):
+        """
+        Args:
+            layer_dimensions: list<integer>
+                - the number of nodes for each layer, including the input and output layers. Has length L.
+            activations: list<(z: array<layer_dimensions[i],1>) => array<layer_dimensions[i],1>>
+                - the activation function to use for each layer. Has length L, the entry at index 0 is not used.
+            activation_derivs: list<(z: array<layer_dimensions[i],1>) => array<layer_dimensions[i],1>>
+                - the activation function derivative to use for each layer. Has length L, the entry at index 0 is not used.
+            cost: (a: array<layer_dimensions[L-1],1>, y: array<layer_dimensions[L-1],1>) => array<layer_dimensions[L-1],>
+                - the cost function. Takes a vector 'a' with the activations from the output layer, and a vector 'y' with the training data outputs, and produces a vector with the errors.
+            cost_deriv: (a: array<layer_dimensions[L-1],1>, y: array<layer_dimensions[L-1],1>) => array<layer_dimensions[L-1],1>
+                - the cost function derivative.
+        """
 
-        self.layer_dimensions = layer_dimensions
-
-        # create array holding the activation functions
-        if activation == None:
-            self.activation = np.append(np.array([relu]*(len(layer_dimensions)-1)), sigmoid)
-            self.activation_deriv = np.append(np.array([relu_deriv]*(len(layer_dimensions)-1)), sigmoid_deriv)
+        if activations == None:
+            self.activations = np.append(np.array([relu]*(len(layer_dimensions)-1)), sigmoid)
+            self.activation_derivs = np.append(np.array([relu_deriv]*(len(layer_dimensions)-1)), sigmoid_deriv)
         else:
-            self.activation = activation
-            self.activation_deriv = activation_deriv
+            self.activations = activations
+            self.activation_derivs = activation_derivs
 
         # set the cost function
         if cost == None:
@@ -60,8 +68,17 @@ class BabyNet:
             self.cost = cost
             self.cost_deriv = cost_deriv
 
-        self.params = self.initialise_params(layer_dimensions)
+        self.layer_dimensions = layer_dimensions
+        self.L = len(layer_dimensions)
 
+        # initialize with None at index 0, as there are no weights or biases for layer 0
+        self.weights = [None]
+        self.biases = [None]
+        for l in range(1, self.L):
+            # a matrix with layer_dimensions[l] rows and layer_dimensions[l-1] columns
+            self.weights.append(np.random.randn(layer_dimensions[l], layer_dimensions[l-1])*0.01)
+            # a vector with layer_dimensions[l] rows
+            self.biases.append(np.zeros((layer_dimensions[l], 1)))
 
 #######################################################
         #self.cost_track = [] # save costs to check updating
@@ -86,126 +103,57 @@ class BabyNet:
         #self.cost_track.append(cost)
 #######################################################
 
-
-    def initialise_params(self, layer_dimensions):
-        """ Initialise parameter dictionary.
-
-        Args:
-            layer_dimensions (python array): contains the dimensions of each layer in the network
-
-        Returns:
-            params (python dictionary): contains parameters from W1 & b1 to WL & bL as keys with values to:
-                                        W - weight matrix with shape: (layer_dimensions[l], layer_dimensions[l-1])
-                                            and values between -0.01 and 0.01
-                                        b - bias vector with shape: (layer_dimensions[l], 1)
-                                            and values equal to 0
-        """
-        params = {}
-        L = len(layer_dimensions)  # amount of layers in the neural net
-        for l in range(1, L):
-            params['W' + str(l-1)] = np.random.randn(layer_dimensions[l], layer_dimensions[l-1])*0.01
-            params['b' + str(l-1)] = np.zeros((layer_dimensions[l], 1))
-        return params
-
-    def update_params(self, params, gradients, learning_rate):
-        L = len(params) // 2
-        for l in range(L):
-            params["W" + str(l+1)] = params["W" + str(l+1)] - learning_rate * gradients["dW" + str(l+1)]
-            params["b" + str(l+1)] = params["b" + str(l+1)] - learning_rate * gradients["db" + str(l+1)]
-        return params
-
-    def forward_steps(self, A_prev, W, b, activation):
-        """ Compute Linear step then Activation Function.
-
-        Args:
-            A_prev ([type]): [description]
-            W ([type]): [description]
-            b ([type]): [description]
-            activation ([type]): [description]
-
-        Returns:
-            [type]: [description]
-        """
-        if activation == "sigmoid":
-            Z = W.dot(A_prev) + b
-            linear_cache = (A_prev, W, b)
-            A, activation_cache = sigmoid(Z)
-
-        elif activation == "relu":
-            Z = W.dot(A_prev) + b
-            linear_cache = (A_prev, W, b)
-            A, activation_cache = relu(Z)
-
-        cache = (linear_cache, activation_cache)
-
-        return A, cache
-
-    def forward_prop(self, X, parameters):
-        caches = []
-        A = X
-        L = len(parameters) // 2   # num of layers in the network //2 due to Wb
-
-        # linear -> relu * (the number of defined layers - 1)
-        for l in range(1, L):
-            A, cache = self.forward_steps(A, parameters['W' + str(l)], parameters['b' + str(l)], activation = "relu")
-            caches.append(cache)
-
-        # linear -> sigmoid (final layer)
-        AL, cache = self.forward_steps(A, parameters['W' + str(L)], parameters['b' + str(L)], activation = "sigmoid")
-        caches.append(cache)
-
-        return AL, caches
-
-#################
-
-
     def forward_prop(self, x):
         """
         Args:
-            x (vector of length layer_dimensions[0]): contains the values for the input layer
+            x: array<layer_dimensions[0],1>
+                - training data inputs.
 
         Returns:
-            z is an array of size len(layer_dimensions) where each i'th element is a vector of size layer_dimensions[i]
-            a is just z with the appropriate activation function for each layer applied
+            z: list<array<layer_dimensions[i],1>>
+                - the calculated z for each layer. Has length L.
+            a: list<array<layer_dimensions[i],1>>
+                - the calculated activations for each layer (this is just z with the appropriate activation function for each layer applied). Has length L.
         """
-        z = []
+        z = [x]
         a = [x]
-        lastA = x
-        for layer in range(0, len(self.layer_dimensions) - 1):
-            weights = self.params['W' + str(layer)]
-            bias = self.params['b' + str(layer)]
-            lastZ = (weights @ lastA) + bias
-            lastA = self.activation[layer](lastZ)
-            z.append(lastZ)
-            a.append(lastA)
+        for l in range(1, self.L):
+            currentZ = self.weights[l] @ a[l-1] + self.biases[l]
+            z.append(currentZ)
+            a.append(self.activations[l](currentZ))
         return z, a
 
     def train_one_piece_of_data(self, x, y, learning_rate = 0.01):
+        """
+        Args:
+            x: array<layer_dimensions[0],1>
+            y: array<layer_dimensions[L-1],1>
+            learning_rate: number
+        """
+        # using the maths from http://neuralnetworksanddeeplearning.com/chap2.html
+
+        L = self.L
+
         # feed forward
         z, a = self.forward_prop(x)
 
-        # using the maths from http://neuralnetworksanddeeplearning.com/chap2.html
-        L = len(self.layer_dimensions) - 2
         # compute the delta errors in the output
-        d = [np.multiply(self.cost_deriv(a[L+1], y), self.activation_deriv[L](z[L]))]
+        d = {
+            (L-1): self.cost_deriv(a[L-1], y) * self.activation_derivs[L-1](z[L-1])
+        }
 
         # back propagation: in reverse, for each layer calculate the delta errors
-        # looks weird because this is a backwards loop, but it goes from [0 to L - 2] inclusive
-        for l in range(L - 1, -1, -1):
-            w = self.params['W' + str(l+1)]
-            arg1 = w.T @ d[0]
-            arg2 = self.activation_deriv[l](z[l])
-            d = [np.multiply(arg1, arg2)] + d
+        # this is a backwards loop, it goes from L - 2 to 0 inclusive
+        for l in range(L - 2, -1, -1):
+            d[l] = (self.weights[l+1].T @ d[l+1]) * self.activation_derivs[l](z[l])
 
         # update the weights and biases
-        for l in range(0, len(self.layer_dimensions) - 1):
-            w = self.params['W' + str(l)]
-            b = self.params['b' + str(l)]
-            print('update', l, w.shape, a[l].shape, d[l].shape)
-            deltaW = a[l] * d[l]
-            deltaB = d[l]
-            self.params['W' + str(l)] = w - learning_rate * deltaW
-            self.params['b' + str(l)] = b - learning_rate * deltaB
+        for l in range(1, L):
+            # row j, col k of dw should be = a[l-1][k] * d[l][j]
+            dw = a[l-1].T * d[l]
+            db = d[l]
+            self.weights[l] = self.weights[l] - learning_rate * dw
+            self.biases[l] = self.biases[l] - learning_rate * db
 
 
 def sigmoid(z):
@@ -223,11 +171,14 @@ def relu_deriv(z):
 def squared_error_cost(a, y):
     """
         Args:
-            a is a vector of output predictions
-            y is a vector of training data outputs
+            a: array<n,1>
+                - activations from the final layer.
+            y: array<n,1>
+                - training data outputs.
 
         Returns:
-            a vector where each element is the squared difference
+            : array<n,1>
+                - the squared differences.
     """
     return 0.5*(a - y)**2
 
@@ -239,33 +190,33 @@ def squared_error_cost_deriv(a, y):
 
 train_x, train_y, test_x, test_y, classes = load_cat_data()
 # reshape the training and test examples
-train_x_flat = train_x.reshape(train_x.shape[0], -1).T
-test_x_flat = test_x.reshape(test_x.shape[0], -1).T
+train_x_flat = train_x.reshape(train_x.shape[0], -1)   # shape: (209, 12288)
+test_x_flat = test_x.reshape(test_x.shape[0], -1)      # shape: (50, 12288)
 
 # standardize data to values between 0 and 1
-standardized_train_x = train_x_flat/train_x_flat.max()
-standardized_test_x = test_x_flat/train_x_flat.max()
+standardized_train_x = train_x_flat/train_x_flat.max() # shape: (209, 12288)
+standardized_test_x = test_x_flat/train_x_flat.max()   # shape: (50, 12288)
+
 net = BabyNet([12288,8,1])
 
-for x, y in zip(standardized_train_x.T, train_y.T):
-    net.train_one_piece_of_data(np.array([x]).T, np.array([y]).T)
+for x, y in zip(standardized_train_x, train_y):
+    x = np.array([x]).T                                # shape: (12288, 1)
+    y = np.array([y]).T                                # shape: (1, 1)
+    net.train_one_piece_of_data(x, y)
 
     # accuracy:
     totalTrainError = 0
     totalTestError = 0
-    for trainX, trainY in zip(standardized_train_x.T, train_y.T):
-        # print(net.forward_prop(trainX).shape)
-        z, a = net.forward_prop(np.array([trainX]))
-        #print(len(a))
-        #print(a[0].shape)
-        #print(a[1].shape)
-        #print(a[2].shape)
-        error = trainY[0] - a
-        totalTrainError += error
+    for trainX, trainY in zip(standardized_train_x, train_y):
+        trainX = np.array([trainX]).T                  # shape: (12288, 1)
+        trainY = np.array([trainY]).T                  # shape: (1, 1)
+        z, a = net.forward_prop(trainX)
+
+        totalTrainError += squared_error_cost(a[2], trainY)[0]
     #for testX, testY in zip(standardized_test_x.T, test_y.T):
     #    error = testY[0] - net.forward_prop(testX)
     #    totalTestError += error
 
     #testAccuracy = totalTestError / len(test_y)
     trainAccuracy = totalTrainError / len(train_y)
-    #print('Train Acc: ', trainAccuracy)
+    print('Train Acc: ', trainAccuracy)
